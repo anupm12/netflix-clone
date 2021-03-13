01f29bda9ed3ee26e2a612e3f1bfab8373dd1a1c
anupm12 <shuklaanupam18@gmail.com>
2021-03-13 21:25:11 +0530

    init

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="flex w-full h-screen justify-center items-center">
+        <p>React typescript tailwind boilerplate.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
