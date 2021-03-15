4574e8a60d8f3a9848f4673c6060081e8796682f
anupm12 <shuklaanupam18@gmail.com>
2021-03-15 12:23:47 +0530

    Jumbotron

--- src/app.tsx
+++ src/app.tsx
@@ -9,13 +9,13 @@ function App() {
         <Jumbotron key={item.id} direction={item.direction}>
           <div className="flex w-full py-5 items-center justify-center border-b-2 border-gray-900">
             <div
-              className={`flex ${item.direction} items-center justify-between md:w-3/5`}
+              className={`flex sm:${item.direction} flex-col items-center justify-center md:w-3/5`}
             >
-              <div className="w-1/2">
-                <p className="text-3xl font-bold p-5">{item.title}</p>
-                <p className="text-lg px-5">{item.subTitle}</p>
+              <div className="w-full sm:w-1/2">
+                <p className="text-3xl font-extrabold p-5">{item.title}</p>
+                <p className="text-sm font-normal px-5">{item.subTitle}</p>
               </div>
-              <div className="w-1/2">
+              <div className="w-full sm:w-1/2 flex justify-center">
                 <img className="h-full" src={item.image} alt={item.alt} />
               </div>
             </div>
