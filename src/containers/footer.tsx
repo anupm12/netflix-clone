2648b2ccaa0f725f69f89a76e497fd2b52dcc0e2
anupm12 <shuklaanupam18@gmail.com>
2021-03-21 00:44:41 +0530

    Footer responsive

--- src/containers/footer.tsx
+++ src/containers/footer.tsx
@@ -9,63 +9,114 @@ const FooterContainer = () => {
           <div className="pb-10">
             <p className="text-lg text-gray-600">Questions? Conact us.</p>
           </div>
-          <div className="grid grid-cols-4 gap-7">
+          <div className="grid grid-cols-2 sm:grid-cols-4 gap-7">
             <div>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 FAQ
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Investor Relations
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Ways to Watch
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Corporate Operations
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Netlix originals
               </a>
             </div>
             <div>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Help Centre
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Jobs
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Terms of Use
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Contact Us
               </a>
             </div>
             <div>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Account
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Reedem Gift Cards
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Privacy
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Speed Test
               </a>
             </div>
             <div>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Media Centre
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Buy Gift Cards
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Cookie Preferences
               </a>
-              <a href="#" className="block pb-5 text-sm text-gray-600">
+              <a
+                href="#"
+                className="block pb-5 text-xs sm:text-sm text-gray-600"
+              >
                 Legal Notices
               </a>
             </div>
