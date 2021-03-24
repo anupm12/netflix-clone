4b3b4eae0d2c2f4924b2346b0b45258b882d5972
anupm12 <shuklaanupam18@gmail.com>
2021-03-24 13:11:37 +0530

    Sign in component

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,11 +1,79 @@
 import { ReactNode } from "react";
+import {
+  Container,
+  Error,
+  Base,
+  Title,
+  Text,
+  TextSmall,
+  Link,
+  Input,
+  Submit,
+} from "./styles/form";
 
-interface FormProps {
+interface IForm {
   children: ReactNode;
 }
 
-const Form: React.FC<FormProps> = ({ children }) => {
-  return <>{children}</>;
+interface FormType {
+  Base: React.FC<{
+    onSubmit: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    method: string;
+  }>;
+  Error: React.FC<IForm>;
+  Title: React.FC<IForm>;
+  Text: React.FC<IForm>;
+  TextSmall: React.FC<IForm>;
+  Link: React.FC<{
+    to: String;
+  }>;
+  Input: React.FC<{
+    type: String;
+    placeholder: String;
+    value: String;
+    onChange: (value: React.SetStateAction<string>) => void;
+    autoComplete?: String;
+  }>;
+  Submit: React.FC<{
+    disabled: Boolean;
+    type: String;
+  }>;
+}
+
+const Form: React.FC<IForm> & FormType = ({ children, ...restProps }) => {
+  return <Container>{children}</Container>;
+};
+
+Form.Base = function FormBase({ children, ...restProps }) {
+  return <Base>{children}</Base>;
+};
+
+Form.Error = function FormError({ children, ...restProps }) {
+  return <Error {...restProps}>{children}</Error>;
+};
+
+Form.Title = function FormTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Form.Text = function FormText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Form.TextSmall = function FormTextSmall({ children, ...restProps }) {
+  return <TextSmall>{children}</TextSmall>;
+};
+
+Form.Link = function FormLink({ children, ...restProps }) {
+  return <Link>{children}</Link>;
+};
+
+Form.Input = function FormInput({ children, ...restProps }) {
+  return <Input>{children}</Input>;
+};
+
+Form.Submit = function FormSubmit({ children, ...restProps }) {
+  return <Submit>{children}</Submit>;
 };
 
 export default Form;
--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,14 +1,59 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from "react";
+import { Form } from "../components";
 import FooterContainer from "../containers/footer";
-import FormContainer from "../containers/form";
 import HeaderContainer from "../containers/header";
+import * as ROUTES from "../constants/routes";
 
 const SignIn = () => {
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid: Boolean = password === "" || emailAddress === "";
+
+  const handleSignIn = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <>
       <HeaderContainer>
-        <FormContainer />
-        <FooterContainer />
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              type="text"
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }: any) => setEmailAddress(target.value)}
+            />
+
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              value={password}
+              autoComplete="off"
+              onChange={({ target }: any) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign IN
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            New to Netflix?{" "}
+            <Form.Link to={ROUTES.SIGN_UP}>Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you are not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
       </HeaderContainer>
+      <FooterContainer />
     </>
   );
 };
